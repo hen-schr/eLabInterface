db3af910961e6d58acf4d51efb84caf381efd909
colorful-owl <henschr22@proton.me>
2024-09-24 19:35:15 +0200

    Basic requests now fully working

--- eLabAPI.py
+++ eLabAPI.py
@@ -1,9 +1,31 @@
 from typing import Union, Literal
+import elabapi_python
+from elabapi_python.rest import ApiException
+from tkinter import filedialog
+import os
+
+
+class HelperElabftw:
+    """
+    As found in the API's documentation (https://github.com/elabftw/elabapi-python)
+    """
+    def __init__(self, api_key, api_host_url):
+        # Configure the api client
+        self.configuration = elabapi_python.Configuration()
+        self.configuration.api_key['api_key'] = api_key
+        self.configuration.api_key_prefix['api_key'] = 'Authorization'
+        self.configuration.host = api_host_url
+        self.configuration.debug = False
+        self.configuration.verify_ssl = True
+        # create an instance of the API class
+        self.api_client = elabapi_python.ApiClient(self.configuration)
+        # fix issue with Authorization header not being properly set by the generated lib
+        self.api_client.set_default_header(header_name='Authorization', header_value=api_key)
 
 
 class ELNResponse:
-    def __init__(self):
-        self.response = None
+    def __init__(self, response=None):
+        self.response = response
         self.metadata = {}
         self.tables = None
 
@@ -49,8 +71,22 @@ data: {"received" if self.response is not None else "none"}
 
         return string
 
-    def request(self) -> ELNResponse:
-        pass
+    def request(self, query=None, limit=None) -> Union[ELNResponse, None]:
+        helper = HelperElabftw(self.api_key, self.url)
+        api_client = helper.api_client
+
+        items = elabapi_python.ItemsApi(api_client)
+
+        items_list = items.read_items(limit=limit)
+
+        if items_list is None:
+            return None
+
+        selection = items_list[0]
+
+        response = ELNResponse(response=selection)
+
+        return response
 
     def configure_api(self, api_key=None, url=None, permissions: Literal["read only", "read and write"] = "read only"):
         if api_key is not None:
@@ -62,14 +98,28 @@ data: {"received" if self.response is not None else "none"}
 
         self.ping_api()
 
+    def attach_api_key_from_file(self, file=None):
+        if file is None:
+            file = filedialog.askopenfilename()
+
+        if os.path.exists(file):
+
+            with open(file, "r") as readfile:
+                self.api_key = readfile.read()
+
     def ping_api(self) -> bool:
         """
         Test if the API could be reached with the defined configuration.
         :return: True if communication to API was successful, False if not
         """
-        ping = False
+        test_response = self.request(limit=1)
 
-        return ping
+        if test_response is not None:
+            self.working = True
+        else:
+            self.working = False
+
+        return self.working
 
 
 
--- testing.py
+++ testing.py
@@ -2,4 +2,9 @@ import eLabAPI
 
 importer = eLabAPI.ELNImporter()
 
+importer.attach_api_key_from_file()
+importer.configure_api(url="https://eln.elaine.uni-rostock.de/api/v2/experiments", permissions="read only")
+
+importer.ping_api()
+
 print(importer)
